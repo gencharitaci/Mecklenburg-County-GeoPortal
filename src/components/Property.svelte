6edface510fcee25eeda24beeb09604324a70544
Adem Kurtipek <gencharitaci@gmail.com>
2026-03-26 11:30:38 -0400

    Update: CAMA tables, attributes for API requests

--- src/components/Property.svelte
+++ src/components/Property.svelte
@@ -102,16 +102,16 @@
       })
 
     // ownership
-    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/cama_tb_pubowner?${jsonToURL({
-      columns: 'nme_ownerlastname,nme_ownerfirstname,txt_mailaddr1,txt_mailaddr2,txt_city,txt_state,txt_zipcode',
-      filter: `id_pid = '${$location.pid}'`
+    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/tb_allowners?${jsonToURL({
+      columns: 'lastname,firstname,address_1,address_2,city,state,zipcode',
+      filter: `parcelid = '${$location.pid}'`
     })}`)
       .then(response => response.json())
       .then(data => {
         data.forEach(el => {
           ownerTable.rows.push([
-            `${el.nme_ownerfirstname || ''} ${el.nme_ownerlastname || ''}`,
-            `${el.txt_mailaddr1} ${el.txt_mailaddr2 || ''}<br>${el.txt_city}, ${el.txt_state} ${el.txt_zipcode}`
+            `${el.firstname || ''} ${el.lastname || ''}`,
+            `${el.address_1} ${el.address_2 || ''}<br>${el.city}, ${el.state} ${el.zipcode}`
           ])
         })
       })
@@ -123,19 +123,19 @@
       })
 
     // appraisal
-    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/cama_tb_pubparcelinfo?${jsonToURL({
-      columns: 'txt_taxyear,amt_netbldgvalue,amt_extrafeaturevalue,amt_landvalue,amt_totalvalue',
-      filter: `id_pid = '${$location.pid}'`
+    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/tb_allparceldata?${jsonToURL({
+      columns: 'yearid,totalbuildingvalue,totalyarditemvalue,totallandvalue,totalvalue',
+      filter: `parcelid = '${$location.pid}'`
     })}`)
       .then(response => response.json())
       .then(data => {
         data.forEach(el => {
           appraisalTable.rows.push([
-            el.txt_taxyear,
-            formatMoney(el.amt_netbldgvalue),
-            formatMoney(el.amt_landvalue),
-            formatMoney(el.amt_extrafeaturevalue),
-            formatMoney(el.amt_totalvalue)
+            el.yearid,
+            formatMoney(el.totalbuildingvalue),
+            formatMoney(el.totallandvalue),
+            formatMoney(el.totalyarditemvalue),
+            formatMoney(el.totalvalue)
           ])
         })
       })
@@ -147,18 +147,19 @@
       })
 
     // sale history
-    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/cama_tb_pubsales?${jsonToURL({
-      columns: 'dte_dateofsale,amt_price,txt_deedbook,txt_deedpage',
-      filter: `id_pid = '${$location.pid}'`,
-      sort: 'dte_dateofsale DESC'
+    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/tb_historicalsales?${jsonToURL({
+      columns: 'saledate,saleprice,legalreference',
+      filter: `parcelid = '${$location.pid}'`,
+      sort: 'saledate DESC'
     })}`)
       .then(response => response.json())
       .then(data => {
         data.forEach(el => {
+          const [deed_book, deed_page] = (el.legalreference || '').split('-');
           saleTable.rows.push([
-            formatDate(el.dte_dateofsale),
-            `<a href="https://meckrod.manatron.com/RealEstate/SearchDetail.aspx?bk=${el.txt_deedbook}&pg=${el.txt_deedpage}&type=BkPg" title="Mecklenburg County Register of Deeds" target="_blank" rel="nofollow noopener noreferrer external">${el.txt_deedbook}-${el.txt_deedpage}</a>`,
-            formatMoney(el.amt_price),
+            formatDate(el.saledate),
+            `<a href="https://meckrod.manatron.com/RealEstate/SearchDetail.aspx?bk=${deed_book}&pg=${deed_page}&type=BkPg" title="Mecklenburg County Register of Deeds" target="_blank" rel="nofollow noopener noreferrer external">${deed_book}-${deed_page}</a>`,
+            formatMoney(el.saleprice),
           ])
         })
       })
@@ -170,18 +171,23 @@
       })
 
     // land use
-    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/cama_tb_publand,cama_ctb_publanduse?${jsonToURL({
-      columns: 'txt_landUse_fulldesc,cnt_landunits,txt_neigh_desc',
-      filter: `cama_tb_publand.cde_landusecode = cama_ctb_publanduse.cde_landuse and id_pid = '${$location.pid}'`
+    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/tb_allparceldata,tb_landvalues?${jsonToURL({
+      columns: 'landuse_description,units,neighborhood_description',
+      filter: `tb_allparceldata.landsize = tb_landvalues.units and tb_allparceldata.landusecode = tb_landvalues.landusecode and parcelid = '${$location.pid}'`
     })}`)
       .then(response => response.json())
       .then(data => {
+        const seen = new Set();
         data.forEach(el => {
-          useTable.rows.push([
-            el.txt_landuse_fulldesc,
-            formatCommas(el.cnt_landunits),
-            el.txt_neigh_desc || ''
-          ])
+          const key = `${el.landuse_description}|${el.units}|${el.neighborhood_description}`;
+          if (!seen.has(key)) {
+            seen.add(key)
+            useTable.rows.push([
+              el.landuse_description,
+              formatCommas(el.units),
+              el.neighborhood_description || ''
+            ])
+          }
         })
       })
       .then(() => {
@@ -192,20 +198,20 @@
       })
 
     // buildings
-    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/cama_tb_pubbuilding?${jsonToURL({
-      columns: 'txt_propertyuse_desc,num_yearbuilt,txt_extwall_desc,num_grossarea,num_bedrooms,cnt_fullbaths,cnt_threeqtrbaths,cnt_halfbaths',
-      filter: `id_pid = '${$location.pid}'`
+    fetch(`https://maps.mecklenburgcountync.gov/dirt/api/v1/query/tb_allbuildings?${jsonToURL({
+      columns: 'buildingtype,yearbuilt,extwall,totalarea,bedrooms,fullbath,threequarterbath,halfbath',
+      filter: `parcelid = '${$location.pid}'`
     })}`)
       .then(response => response.json())
       .then(data => {
         data.forEach(el => {
           buildingTable.rows.push([
-            el.txt_propertyuse_desc,
-            el.num_yearbuilt,
-            el.txt_extwall_desc,
-            formatCommas(el.num_grossarea) + ' Sq. Ft.',
-            el.num_bedrooms,
-            el.cnt_fullbaths + el.cnt_threeqtrbaths + el.cnt_halfbaths
+            el.buildingtype,
+            el.yearbuilt,
+            el.extwall,
+            formatCommas(el.totalarea) + ' Sq. Ft.',
+            el.bedrooms,
+            el.fullbath + el.threequarterbath + el.halfbath
           ])
         })
       })
